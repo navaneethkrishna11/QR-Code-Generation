b3aec33588f8f66b569102f8579a8bac648b7592
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2024-12-16 03:06:57 +0530

    Create qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,16 @@
+import qrcode
+def generate_qr_code(url, filename='website.png'):
+    qr = qrcode.QRCode(
+        version=1,  
+        box_size=10,  
+        border=5  
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_image.save(filename)
+    return filename
+if __name__ == "__main__":
+    website_url = "https://perimeter-visitor.vercel.app/"
+    qr_code_path = generate_qr_code(website_url)
+    print(f"QR Code generated: {qr_code_path}")
